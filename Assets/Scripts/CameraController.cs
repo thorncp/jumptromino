1003a5bd0aed899431597cf7144b5f9c3354839f
Chris Thorn <chris@thorn.co>
2016-10-10 10:15:11 -0700

    More descriptive property name
    `yOffset` is not really indicative of the actual use.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,7 +3,7 @@
 public class CameraController : MonoBehaviour
 {
     public GameObject spawnPoint;
-    public float yOffset = 4;
+    public float birdsEyeYOffset = 4;
 
     private Vector3 startPosition;
     private GameObject piece;
@@ -25,7 +25,7 @@ public class CameraController : MonoBehaviour
             {
                 var birdsEyePostition = new Vector3(
                     piece.transform.position.x,
-                    piece.transform.position.y + offset.y * yOffset,
+                    piece.transform.position.y + offset.y * birdsEyeYOffset,
                     piece.transform.position.z - 2
                 );
                 transform.position = Vector3.MoveTowards(
