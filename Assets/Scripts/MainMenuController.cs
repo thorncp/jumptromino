6a923936de9b3fabe05a83cf8f4e1d0fa5c5cc5b
Chris Thorn <chris@thorn.co>
2016-10-14 14:27:00 -0700

    Main menu tweaks
    * Adds a huge plane to the main menu, with a random piece falling from the
      sky at an interval
    * Adds the Green Screen font and applies it to all UI text

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -6,10 +6,16 @@ public class MainMenuController : MonoBehaviour
 {
     public Button playButton;
     public Button quitButton;
+    public GameObject spawnPoint;
+    public GameObject[] piecePrefabPool;
+    public float pieceTorque = 100;
+
+    private System.Random random = new System.Random();
 
     public void Start()
     {
         Cursor.visible = true;
+        InvokeRepeating("SpawnPiece", 0, 4);
     }
 
     public void Play()
@@ -20,5 +26,26 @@ public class MainMenuController : MonoBehaviour
     public void Quit()
     {
         Application.Quit();
+    }
+
+    private void SpawnPiece()
+    {
+        var index = random.Next(0, piecePrefabPool.Length);
+        var startPiecePrefab = piecePrefabPool[index];
+        var nextPiece = Instantiate(
+            startPiecePrefab,
+            spawnPoint.transform.position + RandomVector(),
+            Random.rotation
+        ) as GameObject;
+        nextPiece.GetComponent<Rigidbody>().AddTorque(RandomVector() * pieceTorque);
+    }
+
+    private Vector3 RandomVector()
+    {
+        return new Vector3(
+            Random.Range(-1.0f, 1.0f),
+            Random.Range(-1.0f, 1.0f),
+            Random.Range(-1.0f, 1.0f)
+        );
     }
 }
